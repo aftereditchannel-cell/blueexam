e792adb67f7baa3d8416a68a45dd82692ce8ba69
aftereditchannel-cell <afteredit.channel@gmail.com>
2026-02-25 10:11:03 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const SCRIPT_URL = "YOUR_SCRIPT_URL";
+
+// تغییر تم
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark"));
+}
+
+window.onload = () => {
+  if (localStorage.getItem("theme") === "true") {
+    document.body.classList.add("dark");
+  }
+};
+
+// تولید examId تصادفی
+function generateId() {
+  return Math.random().toString(36).substr(2, 8);
+}
+
+// ارسال دیتا به گوگل شیت
+async function sendData(data) {
+  document.querySelector(".loader").style.display = "block";
+  await fetch(SCRIPT_URL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+  document.querySelector(".loader").style.display = "none";
+}
